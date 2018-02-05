b68a01d65c5fbf557f44f2eb43b67c2fc4b25067
bb609999 <bb123eric@gmail.com>
2018-02-05 20:40:02 +0800

    Function:add calculate duration

--- app/src/main/java/com/example/cheaptriptravel/MainActivity.java
+++ app/src/main/java/com/example/cheaptriptravel/MainActivity.java
@@ -2,12 +2,89 @@ package com.example.cheaptriptravel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.cheaptriptravel.util.HttpUtil;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String OUHK = "22.316279,%20114.180408";
+
+    private String APM = "22.312441,%20114.225046";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calculateDuration(OUHK,APM);
+
+
+
     }
+
+
+    private void calculateDuration(String startPoint, String endPoint) {
+        HttpUtil.sendOkHttpRequest("https://maps.googleapis.com/maps/api/distancematrix/json" +
+                        "?origins=" + startPoint +
+                        "&destinations=" + endPoint +
+                        "&mode=transit" +
+                        "&key=AIzaSyBGAlVrnlB0BqZxoc7K-PBo3qdwloXTa58",
+                new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        String responseData = response.body().string();
+                        try {
+                            JSONObject jsonObject = new JSONObject(responseData);
+                            String destination_addresses = jsonObject.getString("destination_addresses");
+                            String origin_addresses = jsonObject.getString("origin_addresses");
+
+                            JSONArray rows = jsonObject.getJSONArray("rows");
+
+                            JSONObject elements = new JSONObject();
+
+
+                            for (int i = 0; i < rows.length(); i++) {
+                                elements = rows.getJSONObject(i);
+                            }
+
+                            JSONArray elementArray = elements.getJSONArray("elements");
+
+                            JSONObject elementObj = elementArray.getJSONObject(0);
+
+                            JSONObject durationObj = elementObj.getJSONObject("duration");
+
+                            String duration_value = durationObj.getString("value");
+
+                            Log.d("TEST", destination_addresses + "\n" + origin_addresses + "\n" + rows.toString() + "\n" + elements);
+                            Log.d("TEST", elementArray.toString());
+                            Log.d("TEST", elementObj.toString());
+                            Log.d("TEST", duration_value);
+
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+    }
+
+
+
 }
