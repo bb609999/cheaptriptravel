94fa7087b9234edf980d1847fba736b1d051acda
bb609999 <bb123eric@gmail.com>
2018-02-06 17:36:05 +0800

    Fix calculate

--- app/src/main/java/com/example/cheaptriptravel/MainActivity.java
+++ app/src/main/java/com/example/cheaptriptravel/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.cheaptriptravel;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.cheaptriptravel.util.HttpUtil;
@@ -18,6 +20,10 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -31,9 +37,26 @@ public class MainActivity extends AppCompatActivity implements
 
     private String APM = "22.312441,%20114.225046";
 
+    private String PLAZA = "22.310602,%20114.187868";
+
+    private String GYIN = "22.308235,%20114.185765";
+
+    private String MEGA = "22.320165,%20114.208168";
+
+    private  List<Integer> list = new ArrayList<Integer>();
+
+
+
+
+
     private TextView mTapTextView;
     private TextView mCameraTextView;
     private GoogleMap mMap;
+
+    private int result;
+    private boolean returnOrNot = false;
+
+    private Button calculate_btn;
 
 
 
@@ -41,8 +64,34 @@ public class MainActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
+        //
+       //
 
-        calculateDuration(OUHK,APM);
+        calculate_btn = (Button) findViewById(R.id.calculate_btn);
+        calculate_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //int temp =calculateDuration(OUHK,APM);
+               // Log.d("Next",""+temp);
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //List<String> list = shortestPath(OUHK,APM,PLAZA,GYIN,MEGA);
+                        //Log.d("List",list.toString());
+                        calculateDuration(OUHK,new String[]{APM,PLAZA,GYIN,MEGA});
+                        calculateDuration(OUHK,new String[]{APM,PLAZA,GYIN,MEGA});
+                        Log.d("result", result +"");
+
+                    }
+                }).start();
+
+
+            }
+        });
+
+
 
         mTapTextView = (TextView) findViewById(R.id.tap_text);
         mCameraTextView = (TextView) findViewById(R.id.camera_text);
@@ -79,17 +128,32 @@ public class MainActivity extends AppCompatActivity implements
 
 
 
-    private void calculateDuration(String startPoint, String endPoint) {
-        HttpUtil.sendOkHttpRequest("https://maps.googleapis.com/maps/api/distancematrix/json" +
-                        "?origins=" + startPoint +
-                        "&destinations=" + endPoint +
-                        "&mode=transit" +
-                        "&key=AIzaSyBGAlVrnlB0BqZxoc7K-PBo3qdwloXTa58",
+
+    private List<Integer> calculateDuration(String startPoint, String[] endPoint) {
+
+        list.clear();
+
+        String address = "https://maps.googleapis.com/maps/api/distancematrix/json" +
+                "?origins=" + startPoint +
+                "&destinations=";
+
+
+        for(int i=0;i<endPoint.length;i++) {
+        address += endPoint[i]+"%7C";
+        }
+
+        address += "&mode=transit" +
+                "&key=AIzaSyBGAlVrnlB0BqZxoc7K-PBo3qdwloXTa58";
+
+
+        HttpUtil.sendOkHttpRequest(address,
                 new Callback() {
                     @Override
                     public void onFailure(Call call, IOException e) {
 
                     }
+
+
 
                     @Override
                     public void onResponse(Call call, Response response) throws IOException {
@@ -110,16 +174,31 @@ public class MainActivity extends AppCompatActivity implements
 
                             JSONArray elementArray = elements.getJSONArray("elements");
 
+
                             JSONObject elementObj = elementArray.getJSONObject(0);
+
+                            int[] answer = new int[elementArray.length()];
+
+
+                            for(int i = 0 ;i<elementArray.length();i++){
+                                list.add(elementArray.getJSONObject(i).getJSONObject("duration").getInt("value"));
+                            }
 
                             JSONObject durationObj = elementObj.getJSONObject("duration");
 
-                            String duration_value = durationObj.getString("value");
+                            int duration_value = durationObj.getInt("value");
 
-                            Log.d("TEST", destination_addresses + "\n" + origin_addresses + "\n" + rows.toString() + "\n" + elements);
-                            Log.d("TEST", elementArray.toString());
+                            Log.d("API RETURN", destination_addresses + "\n" + origin_addresses + "\n" + rows.toString() + "\n" + elements);
+                            Log.d("API to element Array", elementArray.toString());
                             Log.d("TEST", elementObj.toString());
-                            Log.d("TEST", duration_value);
+                            Log.d("TEST", duration_value+"");
+
+                             result =  duration_value;
+
+                            Log.d("TEST return:",""+ list.toString());
+
+
+                            returnOrNot = true;
 
 
                         } catch (Exception e) {
@@ -128,7 +207,18 @@ public class MainActivity extends AppCompatActivity implements
 
                     }
                 });
+
+
+        while(returnOrNot == false){
+        }
+        returnOrNot = false;
+
+        return list;
+
+
+
     }
+
 
 
 
